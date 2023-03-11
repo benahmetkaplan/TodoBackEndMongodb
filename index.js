f683406f9fe44f62a9db64e4c794ef9e7754eb4f
Ahmet Kaplan <ahmtkpln96@gmail.com>
2023-03-11 23:37:25 +0300

    events

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import swaggerUi from "swagger-ui-express";
 const swaggerJson = require("./swagger.json");
 
 /** ROUTES */
-import eventRouter from './routers/eventRouter.js';
+import events from './routers/events.js';
 
 /** ENV */
 const port = process.env.PORT;
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/events', eventRouter);
+app.use('/events', events);
 
 app.get('/', (req, res) => {
     res.redirect(301, '/api-docs');
@@ -29,7 +29,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 app.listen(port, () => {
     pgClient.connect(err => {
         if (err) {
-            console.log(`Error: ${err.stack}`);
+            console.log(`Error: ${JSON.stringify(err)}`);
         } else {
             console.log(`Listening: http://${process.env.HOSTNAME}:${port}`);
             console.log(`Swagger UI: http://${process.env.HOSTNAME}:${port}/api-docs`);
