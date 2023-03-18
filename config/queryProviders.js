1d4210d4c1016917a5c81a4a556f22e618e901a5
Ahmet Kaplan <ahmtkpln96@gmail.com>
2023-03-18 19:04:24 +0300

    rev

--- config/queryProviders.js
+++ config/queryProviders.js
@@ -1,84 +1,114 @@
 import mysql from './db.js';
 
 export default {
-    except: (query, fn) => {
-        mysql.query(query, (err, res) => {
-            fn(err, res);
-        });
-    },
-    getDataTable: (table, fn) => {
-        const query = `SELECT * FROM ${table}`;
-        mysql.query(query, (err, res) => {
-            fn(err, res);
-        });
-    },
-    getDataRow: (table, id, fn) => {
-        const query = `SELECT * FROM ${table} WHERE id = ${id}`;
-        mysql.query(query, (err, res) => {
-            if (res.length > 0) {
-                fn(err, res[0]);
-            }else{
-                fn(err, res);
-            }
-        });
-    },
-    getDataCell: (table, cell, id, fn) => {
-        const query = `SELECT ${cell} FROM ${table} WHERE id = ${id}`;
-        mysql.query(query, (err, res) => {
-            if (res.length > 0) {
-                fn(err, res[0][cell]);
-            }else{
-                fn(err, res);
-            }
-        });
-    },
-    insertRow: (table, cols, fn) => {
-        let values = [];
-        let query = `INSERT INTO ${table} (`;
-        Object.keys(cols).forEach(function (key, i) {
-            if (i > 0) {
-                query += ",";
-            }
-            query += key;
-            values.push(cols[key]);
-        });
-        query += ") VALUES(";
-        Object.keys(cols).forEach(function (key, i) {
-            if (i > 0) {
-                query += ",";
-            }
-            query += "?";
-        });
-        query += ")";
-        mysql.query(query, values, (err, res) => {
-            if (!err) {
-                if (res.insertId !== undefined && res.insertId > 0) {
-                    fn(err, res.insertId);
-                } else {
-                    fn(err, res);
+    exec: (query) => {
+        return new Promise(function (resolve, reject) {
+            mysql.query(query, (err, res) => {
+                if (!err) {
+                    resolve(res);
+                }else{
+                    reject(err);
                 }
-            } else {
-                fn(err, res);
-            }
+            });
         });
     },
-    updateRow: (table, id, cols, fn) => {
-        let query = "UPDATE `"+table+"` SET ";
-        Object.keys(cols).forEach(function (key, i) {
-            if (i > 0) {
-                query += ",";
-            }
-            query += "`"+key+"`='"+cols[key]+"'";
-        });
-        query += " WHERE id = " + id;
-        mysql.query(query, (err, res) => {
-            fn(err, res);
+    table: (table) => {
+        return new Promise(function (resolve, reject) {
+            const query = `SELECT * FROM ${table}`;
+            mysql.query(query, (err, res) => {
+                if (!err) {
+                    resolve(res);
+                }else{
+                    reject(err);
+                }
+            });
         });
     },
-    deleteRow: (table, id, fn) => {
-        let query = "DELETE FROM `"+table+"` WHERE id = "+ id;
-        mysql.query(query, (err, res) => {
-            fn(err, res);
+    row: (table, id) => {
+        return new Promise(function (resolve, reject) {
+            const query = `SELECT * FROM ${table} WHERE id = ${id}`;
+            mysql.query(query, (err, res) => {
+                if (res.length > 0) {
+                    resolve(res[0]);
+                }else{
+                    reject(err);
+                }
+            });
+        });
+    },
+    cell: (table, cell, id) => {
+        return new Promise(function (resolve, reject) {
+            const query = `SELECT ${cell} FROM ${table} WHERE id = ${id}`;
+            mysql.query(query, (err, res) => {
+                if (res[0][cell] !== undefined) {
+                    resolve(res[0][cell]);
+                }else{
+                    reject(err);
+                }
+            });
+        });
+    },
+    insert: (table, cols) => {
+        return new Promise(function (resolve, reject) {
+            let values = [];
+            let query = `INSERT INTO ${table} (`;
+            Object.keys(cols).forEach(function (key, i) {
+                if (i > 0) {
+                    query += ",";
+                }
+                query += key;
+                values.push(cols[key]);
+            });
+            query += ") VALUES(";
+            Object.keys(cols).forEach(function (key, i) {
+                if (i > 0) {
+                    query += ",";
+                }
+                query += "?";
+            });
+            query += ")";
+            mysql.query(query, values, (err, res) => {
+                if (!err) {
+                    if (res.insertId !== undefined && res.insertId > 0) {
+                        resolve(res);
+                    } else {
+                        reject(err);
+                    }
+                } else {
+                    reject(err);
+                }
+            });
+        });
+    },
+    update: (table, id, cols) => {
+        return new Promise(function (resolve, reject) {
+            let query = "UPDATE `"+table+"` SET ";
+            Object.keys(cols).forEach(function (key, i) {
+                if (i > 0) {
+                    query += ",";
+                }
+                query += "`"+key+"`='"+cols[key]+"'";
+            });
+            query += " WHERE id = " + id;
+            mysql.query(query, (err, res) => {
+                if (!err) {
+                    resolve(res);
+                }else{
+                    reject(err);
+                }
+            });
+        });
+    },
+    delete: (table, id) => {
+        return new Promise(function (resolve, reject) {
+            let query = "DELETE FROM `"+table+"` WHERE id = "+ id;
+            mysql.query(query, (err, res) => {
+                if (!err) {
+                    resolve(res);
+                }else{
+                    reject(err);
+                }
+            });
         });
     }
 };
--- routers/events.js
+++ routers/events.js
@@ -5,206 +5,44 @@ const router = express.Router();
 const table = "events";
 
 // GET: List all rows
-router.get('/', (req, res) => {
-    qp.getDataTable(table, (err, result) => {
-        if (!err) {
-            return res.status(200).json({
-                statusCode: 200,
-                message: null,
-                rows: result,
-                row: null,
-                insertId: null
-            });
-        } else {
-            console.log("Error:", err);
-            return res.status(400).json({
-                statusCode: 400,
-                message: err,
-                rows: null,
-                row: null,
-                insertId: null
-            });
-        }
+router.get('/', (request, res) => {
+    qp.table(table)
+    .then(data => {
+        return res.status(200).json({
+            statusCode: 200,
+            message: "Successfully",
+            rows: data,
+            row: null,
+            insertId: null
+        });
+    })
+    .catch(data => {
+        console.log("Error:", data);
+        return res.status(400).json({
+            statusCode: 400,
+            message: "Rows not found!",
+            rows: null,
+            row: null,
+            insertId: null
+        });
     });
 });
 
 // GET: Row by id
 router.get('/:id', (req, res) => {
     const { id } = req.params;
-    qp.getDataRow(table, id, (err, result) => {
-        if (!err) {
-            if (typeof result === 'object') {
-                return res.status(200).json({
-                    statusCode: 200,
-                    message: null,
-                    rows: null,
-                    row: result,
-                    insertId: null
-                });
-            } else {
-                return res.status(400).json({
-                    statusCode: 400,
-                    message: "Row not found!",
-                    rows: null,
-                    row: null,
-                    insertId: null
-                });
-            }
-        } else {
-            console.log("Error:", err);
-            return res.status(400).json({
-                statusCode: 400,
-                message: err,
+    qp.row(table, id)
+    .then(data => {
+        if (typeof data === "object") {
+            return res.status(200).json({
+                statusCode: 200,
+                message: "Successfully",
                 rows: null,
-                row: null,
+                row: data,
                 insertId: null
             });
-        }
-    });
-});
-
-// POST: Create row
-router.post('/', (req, res) => {
-    qp.insertRow(table, req.body, (err, result) => {
-        if (!err) {
-            if (typeof result === 'number') {
-                return res.status(200).json({
-                    statusCode: 200,
-                    message: "Successfully inserted",
-                    rows: null,
-                    row: null,
-                    insertId: result
-                });
-            } else {
-                return res.status(400).json({
-                    statusCode: 400,
-                    message: "İnsert failed",
-                    rows: null,
-                    row: null,
-                    insertId: null
-                });
-            }
         } else {
-            console.log("Error:", err);
             return res.status(400).json({
-                statusCode: 400,
-                message: err,
-                rows: null,
-                row: null,
-                insertId: null
-            });
-        }
-    });
-});
-
-// PUT: Row by id
-router.put('/:id', (req, res) => {
-    const { id } = req.params;
-    qp.updateRow(table, id, req.body, (err, result) => {
-        if (!err) {
-            if (result.affectedRows === 1) {
-                return res.status(200).json({
-                    statusCode: 200,
-                    message: "Successfully updated",
-                    rows: null,
-                    row: null,
-                    insertId: null
-                });
-            } else {
-                return res.status(400).json({
-                    statusCode: 400,
-                    message: "Update failed",
-                    rows: null,
-                    row: null,
-                    insertId: null
-                });
-            }
-        } else {
-            console.log("Error:", err);
-            return res.status(400).json({
-                statusCode: 400,
-                message: err,
-                rows: null,
-                row: null,
-                insertId: null
-            });
-        }
-    });
-});
-
-// DELETE: Row by id
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    qp.deleteRow(table, id, (err, result) => {
-        if (!err) {
-            if (result.affectedRows === 1) {
-                return res.status(200).json({
-                    statusCode: 200,
-                    message: "Successfully deleted",
-                    rows: null,
-                    row: null,
-                    insertId: null
-                });
-            } else {
-                return res.status(400).json({
-                    statusCode: 400,
-                    message: "Delete failed",
-                    rows: null,
-                    row: null,
-                    insertId: null
-                });
-            }
-        } else {
-            console.log("Error:", err);
-            return res.status(400).json({
-                statusCode: 400,
-                message: err,
-                rows: null,
-                row: null,
-                insertId: null
-            });
-        }
-    });
-});
-
-// PUT: Change statu by id
-router.put('/changeStatu/:id', (request, response) => {
-    const { id } = request.params;
-    qp.getDataCell(table, 'statu', id, (error, result) => {
-        if (!error) {
-            let statu = result;
-            statu = (statu === 1) ? 0 : 1;
-            qp.updateRow(table, id, {statu: statu}, (err, res) => {
-                if (!err) {
-                    if (res.affectedRows === 1) {
-                        return response.status(200).json({
-                            statusCode: 200,
-                            message: "Successfully changed",
-                            rows: null,
-                            row: null,
-                            insertId: null
-                        });
-                    } else {
-                        return response.status(400).json({
-                            statusCode: 400,
-                            message: "Change failed",
-                            rows: null,
-                            row: null,
-                            insertId: null
-                        });
-                    }
-                }else{
-                    return response.status(400).json({
-                        statusCode: 400,
-                        message: "Change failed",
-                        rows: null,
-                        row: null,
-                        insertId: null
-                    });
-                }
-            });
-        }else{
-            return response.status(400).json({
                 statusCode: 400,
                 message: "Row not found!",
                 rows: null,
@@ -212,6 +50,239 @@ router.put('/changeStatu/:id', (request, response) => {
                 insertId: null
             });
         }
+    })
+    .catch(data => {
+        console.log("Error:", data);
+        return res.status(400).json({
+            statusCode: 400,
+            message: "Row not found!",
+            rows: null,
+            row: null,
+            insertId: null
+        });
+    });
+});
+
+// POST: Create row
+router.post('/', (req, res) => {
+    qp.insert(table, req.body)
+    .then(data => {
+        if (data.insertId !== undefined && data.insertId > 0) {
+            qp.row(table, data.insertId)
+            .then(rowData => {
+                if (typeof rowData === "object") {
+                    return res.status(200).json({
+                        statusCode: 200,
+                        message: "Successfully",
+                        rows: null,
+                        row: rowData,
+                        insertId: data.insertId
+                    });
+                } else {
+                    return res.status(400).json({
+                        statusCode: 400,
+                        message: "Row not found!",
+                        rows: null,
+                        row: null,
+                        insertId: null
+                    });
+                }
+            })
+            .catch(data => {
+                console.log("Error:", data);
+                return res.status(400).json({
+                    statusCode: 400,
+                    message: "İnsert failed!",
+                    rows: null,
+                    row: null,
+                    insertId: null
+                });
+            });
+        } else {
+            return res.status(400).json({
+                statusCode: 400,
+                message: "İnsert failed",
+                rows: null,
+                row: null,
+                insertId: null
+            });
+        }
+    })
+    .catch(data => {
+        console.log("Error:", data);
+        return res.status(400).json({
+            statusCode: 400,
+            message: "İnsert failed",
+            rows: null,
+            row: null,
+            insertId: null
+        });
+    });
+});
+
+// PUT: Row by id
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    qp.update(table, id, req.body)
+    .then(data => {
+        if (data.affectedRows === 1) {
+            qp.row(table, id)
+            .then(rowData => {
+                if (typeof rowData === "object") {
+                    return res.status(200).json({
+                        statusCode: 200,
+                        message: "Successfully",
+                        rows: null,
+                        row: rowData,
+                        insertId: null
+                    });
+                } else {
+                    return res.status(400).json({
+                        statusCode: 400,
+                        message: "Row not found!",
+                        rows: null,
+                        row: null,
+                        insertId: null
+                    });
+                }
+            })
+            .catch(data => {
+                console.log("Error:", data);
+                return res.status(400).json({
+                    statusCode: 400,
+                    message: "Update failed!",
+                    rows: null,
+                    row: null,
+                    insertId: null
+                });
+            });
+        } else {
+            return res.status(400).json({
+                statusCode: 400,
+                message: "Update failed",
+                rows: null,
+                row: null,
+                insertId: null
+            });
+        }
+    })
+    .catch(data => {
+        console.log("Error:", data);
+        return res.status(400).json({
+            statusCode: 400,
+            message: "Update failed",
+            rows: null,
+            row: null,
+            insertId: null
+        });
+    });
+});
+
+// DELETE: Row by id
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    qp.delete(table, id)
+    .then(data => {
+        if (data.affectedRows === 1) {
+            return res.status(200).json({
+                statusCode: 200,
+                message: "Successfully",
+                rows: null,
+                row: null,
+                insertId: null
+            });
+        } else {
+            return res.status(400).json({
+                statusCode: 400,
+                message: "Delete failed",
+                rows: null,
+                row: null,
+                insertId: null
+            });
+        }
+    })
+    .catch(data => {
+        console.log("Error:", data);
+        return res.status(400).json({
+            statusCode: 400,
+            message: "Delete failed",
+            rows: null,
+            row: null,
+            insertId: null
+        });
+    });
+});
+
+// PUT: Change statu by id
+router.put('/changeStatu/:id', (request, response) => {
+    const { id } = request.params;
+    qp.cell(table, 'statu', id)
+    .then(data => {
+        let statu = data;
+        statu = (statu === 1) ? 0 : 1;
+        qp.update(table, id, { statu: statu })
+        .then(data => {
+            if (data.affectedRows === 1) {
+                qp.row(table, id)
+                .then(rowData => {
+                    if (typeof rowData === "object") {
+                        return response.status(200).json({
+                            statusCode: 200,
+                            message: "Successfully",
+                            rows: null,
+                            row: rowData,
+                            insertId: null
+                        });
+                    } else {
+                        return response.status(400).json({
+                            statusCode: 400,
+                            message: "Row not found!",
+                            rows: null,
+                            row: null,
+                            insertId: null
+                        });
+                    }
+                })
+                .catch(data => {
+                    console.log("Error:", data);
+                    return response.status(400).json({
+                        statusCode: 400,
+                        message: "Change failed!",
+                        rows: null,
+                        row: null,
+                        insertId: null
+                    });
+                });
+            } else {
+                return response.status(400).json({
+                    statusCode: 400,
+                    message: "Change failed",
+                    rows: null,
+                    row: null,
+                    insertId: null
+                });
+            }
+        })
+        .catch(data => {
+            console.log("Error:", data);
+            return response.status(400).json({
+                statusCode: 400,
+                message: "Change failed",
+                rows: null,
+                row: null,
+                insertId: null
+            });
+        });
+    })
+    .catch(data => {
+        console.log("Error:", data);
+        return response.status(400).json({
+            statusCode: 400,
+            message: "Data not found!",
+            rows: null,
+            row: null,
+            insertId: null
+        });
     });
 });
 
